47385e338c7d99e79b76f428705ff0f9b662d477
Raunaq Vyas <raunaqvyas14@gmail.com>
2021-12-30 18:26:46 -0500

    key changed

--- CliMate/WeatherManger.swift
+++ CliMate/WeatherManger.swift
@@ -15,7 +15,7 @@ protocol WeatherManagerDelegate {
 
 struct WeatherManager {
     
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=#####key&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=&units=metric"
     
     var delegate : WeatherManagerDelegate?
     
